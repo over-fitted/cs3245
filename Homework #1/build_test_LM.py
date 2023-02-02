8c03359c16fa819a0fc2ef81460d4b8f6f159c00
Over Fitted <overfitted@gmail.com>
2023-02-02 13:26:46 +0800

    Adds code documentation

--- Homework #1/build_test_LM.py
+++ Homework #1/build_test_LM.py
@@ -37,6 +37,7 @@ def build_LM(in_file):
     with open(in_file) as f:
         inputStrings = f.readlines()
     for inputString in inputStrings:
+        # Detect language by first 3 characters, then process string with corresponding LM
         language = inputString[0:3]
         if language == "ind":
             indoSum = processString(inputString[11:], indoFreq, indoSum)
@@ -49,6 +50,7 @@ def build_LM(in_file):
             continue
         print("language not found",language)
     
+    # Smoothen LMs by adding one of each seen ngram
     malaySum += len(seenWords)
     indoSum += len(seenWords)
     tamilSum += len(seenWords)
@@ -63,6 +65,7 @@ def build_LM(in_file):
     # check(indoFreq)
     # check(tamilFreq)
 
+# Sum of all probabilities should be ~1
 def check(freq):
     sum = 0.0
     for key in freq.keys():
@@ -70,9 +73,10 @@ def check(freq):
 
     print(sum)
             
-
+# given a language LM and the input string, add the input string ngrams to the LM
 # def processString(inputString: str, langDict: Dict[str, int], langSum: int) -> int:
 def processString(inputString, langDict, langSum):
+    # intialise sliding window buffer
     ngram = []
     ngram[:0] = inputString[0:3]
 
@@ -87,12 +91,15 @@ def processString(inputString, langDict, langSum):
     
     return langSum
     
+# Add one to each ngram count, then divide by the total count of ngrams in the LM
 # def smoothen(word: string, langDict: Dict[str, int], langSum: int) -> None:
 def smoothen(word, langDict, langSum):
     if word not in langDict.keys():
         langDict[word] = 1.0 / langSum
     else:
         langDict[word] = float(langDict[word] + 1) / langSum    
+
+    # Error
     if(langDict[word] == 0):
         print("0 found")
 
@@ -110,9 +117,14 @@ def test_LM(in_file, out_file, LM):
         inputStrings = f.readlines()
     for i in range(len(inputStrings)):
         inputString = inputStrings[i]
+
+        # We use logarithms for the probabilities to avoid small decimals being falsely rounded to 0
+        # The more negative, the smaller the probability
         malayProb = 0.0
         indoProb = 0.0
         tamilProb = 0.0
+
+        # Keep track of ngrams not in training data as a proportion of total ngrams in the input string
         unseen = 0.0
         total = 0.0
 
@@ -133,16 +145,19 @@ def test_LM(in_file, out_file, LM):
 
             ngram.pop(0)
 
-        # decide which is the best language and add the language name to front of string
+        
+        # ngrams not in training data too high, majority of ngrams absent, label as other language
+        if unseen / total > 0.6:
+            inputString = "other " + inputString
+            inputStrings[i] = inputString
+            continue
+
+        # decide which is the closest language and add the language name to front of string
         best = max(indoProb, malayProb, tamilProb)
 
         # debugging info
         # print(i, "unseen", unseen / total,"best", best)
 
-        if best == 0.0 or unseen / total >= 0.6:
-            inputString = "other " + inputString
-            inputStrings[i] = inputString
-            continue
         if best == malayProb:
             inputString = "malaysian " + inputString
             inputStrings[i] = inputString
